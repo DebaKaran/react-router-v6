61f3d884612d05cbd8c1868f125ed8442ffcacaf
Tarachand90 <tarachand90@gmail.com>
2025-06-18 00:00:09 +0530

    chore: integrate Prettier with ESLint, add formatting rules, and auto-fix existing code style issues

--- introduction_to_router/eslint.config.js
+++ introduction_to_router/eslint.config.js
@@ -1,31 +1,29 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
-
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import pluginPrettier from 'eslint-plugin-prettier';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
+  js.configs.recommended, // <--- as object, inside array
+  tseslint.configs.recommended,
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    //plugins: { js, react: pluginReact },
-    //extends: ["js/recommended"],
-    plugins: { react: pluginReact },
-   ...js.configs.recommended,
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    plugins: { react: pluginReact, prettier: pluginPrettier },
     languageOptions: {
-      globals: globals.browser
+      globals: globals.browser,
     },
     rules: {
-      ...pluginReact.configs.flat.recommended.rules, // load rules here
-      "react/react-in-jsx-scope": "off", 
-      "@typescript-eslint/no-unused-vars": "warn"
+      ...pluginReact.configs.flat.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'prettier/prettier': 'error',
     },
     settings: {
       react: {
-        version: "detect" // avoid the "version not specified" warning
-      }
-    }
+        version: 'detect',
+      },
+    },
   },
-  tseslint.configs.recommended,
-  
 ]);
